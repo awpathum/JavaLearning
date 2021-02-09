90409d07fce04c1f54a5552d76f9682923c4b48d
pathum <awpathum@gmail.com>
2021-02-09 22:53:37 +0530

    variable hiding

---
+++ src/Garage.java
@@ -0,0 +1,32 @@
+
+class Automobile {
+    public int x = 10;
+
+    public void sayHello() {
+        System.out.println("Hello From AutoMobile");
+    }
+}
+
+class Car extends Automobile {
+    private int x = 20;
+
+    public void sayHello() {
+        System.out.println("Hello From Car");
+    }
+}
+public class Garage{
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.sayHello();
+        System.out.println("x = " + car.x);
+        Automobile automobile = (Automobile) car;
+        System.out.println("x = " + automobile.x);
+
+        /**
+         * In this case x is hidden by Car class. Garage cannot access public x of Automobile class. (This is method hiding).
+         * If we remove private access modifier of Car's x, program will compile and print "x = 20" in line 21.
+         * We cannot access Automobile's x variable without casting Car to Automobile.
+         * After casting in line 22, program will print "x = 10" in line 23. (need to comment line 21).
+         */
+    }
+}
